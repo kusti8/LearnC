a2eb1c70ce56ed02ff221c74744ed2188b8c72e1
kusti8 <kusti8@gmail.com>
2015-08-22 10:07:12 -0400

    Start Ch3 exercises, not finished

---
+++ Principles/Ch3/exercises.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Which exercise do you want to use?\n";
+    int ex;
+    cin >> ex;
+
+    if (ex == 2){
+        cout << "\nConvert miles to kilometers: miles:\n";
+        double miles;
+        cin >> miles;
+        cout << "That is " << miles*1.609 << " kilometers\n";
+        return 0;
+    }
+
+    if (ex == 3){
+        
+    }
+}
