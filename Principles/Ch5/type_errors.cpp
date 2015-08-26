fab9b74c14b56f58748044688e7257d458843c53
kusti8 <kusti8@gmail.com>
2015-08-26 08:51:16 -0400

    Add type error testing

---
+++ Principles/Ch5/type_errors.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+int area(int length, int width) {
+    return length*width;
+}
+
+int main() {
+    /*
+    int x0 = arena(7);
+    int x1 = area(7);
+    int x2 = area("seven", 7);
+    */
+}
