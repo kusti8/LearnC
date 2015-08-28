368fc77853ad67e27454a1ceff15140addb9f7c5
kusti8 <kusti8@gmail.com>
2015-08-28 15:47:19 -0400

    Add temperature calc

---
+++ Principles/Ch5/temperature_calc.cpp
@@ -0,0 +1,23 @@
+#include "std_lib_facilities.h"
+
+int main() {
+
+    vector<double> temps;              // temperatures
+    for (double temp; cin >> temp;)    // read and put into temps
+        temps.push_back(temp);
+
+    double sum = 0;
+    double high_temp = 0;
+    double low_temp = 0;
+
+    for (int x : temps)
+    {
+        if(x > high_temp) high_temp = x;   // find high
+        if(x < low_temp) low_temp = x;     // find low
+        sum += x;                          // compute sum
+    }
+
+    cout << "High temperature: " << high_temp << '\n';
+    cout << "Low temperature: " << low_temp << '\n';
+    cout << "Average temperature: " << sum/temps.size() << '\n';
+}
