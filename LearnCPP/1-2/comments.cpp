3d57c6050c28de8de4fa4a28db7d731206510560
kusti8 <kusti8@gmail.com>
2016-10-18 17:33:12 -0400

    Add 1.2 Comments

---
+++ LearnCPP/1-2/comments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello world!" << std::endl; // This is a comment
+
+  // Usually put before code
+  std::cout << "HI" << std::endl;
+  std::cout << "It works" << std::endl;
+
+  /*
+  A multiline comment
+  See? */
+
+  //Fully explain everything, not the statement, but the reason
+
+  // In Atom, do CTRL+/ to comment selection
+}
