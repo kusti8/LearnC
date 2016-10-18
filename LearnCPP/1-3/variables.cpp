986d4381cf6cd4690693d064ba39ac5af6eeab38
kusti8 <kusti8@gmail.com>
2016-10-18 17:42:49 -0400

    Add 1.3 Variables

---
+++ LearnCPP/1-3/variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  int x; // variable definition
+  x = 5; // variable assignment
+  // Or int x = 5;
+  std::cout << x << std::endl; // prints the value of x, which is 5
+
+  // Evaluate into y
+  int y;
+  y = 4;
+  y = 2+5;
+
+  // Evaluate into x
+  x = y;
+  x = x;
+  x = x + 1;
+
+  int z;
+  std::cout << z << std::endl;
+  return 0;
+}
